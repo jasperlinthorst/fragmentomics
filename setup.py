adb979ff8577f34add1aae9f515db1510a40a3b3
jasperlinthorst <jasper.linthorst@gmail.com>
2024-08-27 22:07:23 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cfstats', author="Jasper Linthorst", author_email="jasper.linthorst@gmail.com",
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cfstats=cfstats.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
+
