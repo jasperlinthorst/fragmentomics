4b0ba6f01dd6b5bc8fdc9736a95b1dc9b28c7a4d
jasperlinthorst <jasper.linthorst@gmail.com>
2026-02-11 11:56:04 +0100

    fix projecting multiple samples on fragmentome

--- cfstats/dnase1l3.py
+++ cfstats/dnase1l3.py
@@ -6,16 +6,17 @@ import numpy as np
 import sys
 
 from cfstats import csm, fszd, fpends
+import os
 
 def dnase1l3(args):
     (pca,clfb,clfgt,reg)=pickle.load(open(args.clf, 'rb'))
 
-    args.k=8
+    args.k=4
     args.norm='rpx'
     args.x=1000000
     args.purpyr=False
     
-    f=np.array(list(cleavesitemotifs(args, cmdline=False, ))).reshape(1,-1)
+    f=np.array(list(csm.cleavesitemotifs(args, cmdline=False, ))).reshape(1,-1)
 
     fp=pca.transform(f)
 
@@ -34,43 +35,50 @@ def plot_fragmentome(args):
 
     (reducer,embedding,k)=pickle.load(open(args.mapping, 'rb'))
 
-    args.k=4
-    args.norm='rpx'
-    
-    args.exclflag=1024
-    args.mapqual=0
 
-    args.x=1000000
+    args.k=4
+    args.norm='freq'
+    
+    args.exclflag=3852
+    args.mapqual=60
+
+    #args.x=1000000
     args.purpyr=False
     args.uselexsmallest=False
     args.useref=False
     args.insertissize=True
-    args.lower=60
-    args.upper=600
+    args.lower=0
+    args.upper=1000
     args.bamlist=None
 
-    Xfszd=np.array(fszd(args, cmdline=False, )).reshape(1,-1)
+    Xfszd=np.array(fszd.fszd(args, cmdline=False, ))#.reshape(1,-1)
     
     args.mapqual=60
     args.exclflag=3852
-    Xcsm=np.array(cleavesitemotifs(args, cmdline=False, )).reshape(1,-1)
-    print("csm",Xcsm,Xcsm.sum())
+    Xcsm=np.array(csm.cleavesitemotifs(args, cmdline=False, ))#.reshape(1,-1)
+    #print("csm",Xcsm,Xcsm.sum())
 
-    Xsem=np.array(_5pends(args, cmdline=False, )).reshape(1,-1)
-    print("sem",Xsem,Xsem.sum())
+    Xsem=np.array(fpends._5pends(args, cmdline=False, ))#.reshape(1,-1)
+    #print("sem",Xsem,Xsem.sum())
 
     f=np.concatenate((Xfszd,Xcsm,Xsem),axis=1)
 
-    print(f.shape)
+    print("Calculated feature set for cramfiles: ",f.shape)
     # print(f)
 
     fp=reducer.transform(f)
     print(fp)
 
     plt.scatter(embedding[:,0],embedding[:,1],c='blue',s=5,alpha=0.5)
-    plt.scatter(fp[:,0],fp[:,1],c='red',s=5,alpha=1)
+    plt.scatter(fp[:,0],fp[:,1],c='red',s=10,alpha=1)
     # plt.show()
     if args.outfile is None:
-        args.outfile="fragmentome.png"
+        if len(args.samfiles)==1:
+            args.outfile=os.path.basename(args.samfiles[0]).rstrip('cram').rstrip('bam').rstrip('sam')+"fragmentome.png"
+        else:
+            import uuid
+            args.outfile=uuid.uuid4().hex[:8]+"fragmentome.png"
+
+    sys.stderr.write(f"Writing fragmentome plot to: {args.outfile}")
     plt.savefig(args.outfile)
     plt.close()
